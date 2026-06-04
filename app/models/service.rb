fef27599e692e2e1b1832edd0d8206ed8d0f0f01
fellowbeard <virgovibe@gmail.com>
2026-06-04 12:29:25 -0500

    updates to remove cient from service schema table, added client form, added buttons to userdashboard

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,5 @@
 class Service < ApplicationRecord
   belongs_to :user
-  belongs_to :client
   has_many :appointment_services, dependent: :destroy
   has_many :appointments, through: :appointment_services
 end
